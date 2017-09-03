6903389315133009123208da22305e6ab8cb6bc5
System <none>
2017-09-03 13:03:05 +0000

    Initial Commit

---
+++ app/models/users.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var mongoose = require('mongoose')
+var bcrypt = require('bcrypt-nodejs')
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+
+  local: {
+    email: String,
+    password: String
+  }
+
+  //change user collection to use one sandbox DB for multiple projects
+}, { collection: 'THISPROJECTUser' })
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+// checking if password is valid
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema)
